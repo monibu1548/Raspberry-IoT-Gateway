8132b447be86fb1d920d7fb31ceb088ab599de9a
monibu1548 <monibu1548@gmail.com>
2015-08-09 15:34:22 +0900

    Add more statement

--- WebMonitering/JS/loadData.js
+++ WebMonitering/JS/loadData.js
@@ -2,8 +2,18 @@ var tempData101 = $('#temp101');
 var humData101 = $('#hum101');
 var useData101 = $('#user101');
 
-$.ajax(
-	url : 'localhost:3000/1',
-	function(success){
+var tempData102 = $('#temp102');
+var humData102 = $('#hum102');
+var useData102 = $('#user102');
+
+var loadData = function(){
+
+
+	$.ajax(
+		url : 'localhost:3000/1',
+		function(success){
 		
-});
+	});
+}
+
+
